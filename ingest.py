4d836196c764406bc01881758906e9778a9e46a9
Sudarshan Koirala <sudarshan@Sudarshans-MacBook-Pro.local>
2024-02-17 11:10:13 +0200

    code modification and cleaning

--- ingest.py
+++ ingest.py
@@ -1,7 +1,7 @@
 import os
 import warnings
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import (
     UnstructuredURLLoader
 )
@@ -19,20 +19,20 @@ def create_vector_database():
     """
     Creates a vector database using document loaders and embeddings.
 
-    This function loads data from PDF, markdown and text files in the 'data/' directory,
+    This function loads urls,
     splits the loaded documents into chunks, transforms them into embeddings using OllamaEmbeddings,
     and finally persists the embeddings into a Chroma vector database.
 
     """
     # Initialize loader
-    urls = ['https://openai.com/sora', 'https://www.langchain.com/']
+    urls = ['https://docs.gpt4all.io/', 'https://ollama.com/library/llama2']
     
     url_loader = UnstructuredURLLoader(urls = urls, show_progress_bar=True)
     loaded_documents = url_loader.load()
     #len(loaded_documents)
 
     # Split loaded documents into chunks
-    text_splitter = CharacterTextSplitter(separator='\n', chunk_size=1000, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunked_documents = text_splitter.split_documents(loaded_documents)
     #len(chunked_documents)
     #chunked_documents[0]
